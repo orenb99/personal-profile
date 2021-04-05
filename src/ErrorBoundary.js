4c7b2afe7e5de22043d83519dcee333a44da0754
orenb99 <77238204+orenb99@users.noreply.github.com>
2021-04-05 22:13:04 +0300

    done with notes

--- src/ErrorBoundary.js
+++ src/ErrorBoundary.js
@@ -1,22 +1,25 @@
 import React, { Component } from "react";
 import { Link, Redirect } from "react-router-dom";
-
+//shit not working
 export default class ErrorBoundary extends Component {
   constructor(props) {
     super(props);
     this.state = { hasError: false };
   }
   static getDerivedStateFromError(error) {
-    return { hasError: true };
+    this.state.hasError = true;
   }
   componentDidCatch(error, errorInfo) {
-    console.log(errorInfo);
+    alert(error);
   }
   render() {
+    console.log(this.state);
     if (this.state.hasError) {
-      <div className="main">
-        <h1>something went wrong</h1>
-      </div>;
+      return (
+        <div className="main">
+          <h1>something went wrong</h1>
+        </div>
+      );
     }
     return this.props.children;
   }
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { Link, Redirect } from "react-router-dom";
 import pic from "../imgs/profile-pic.png";
 import { ThemeContext, UserContext } from "../App";
+import ErrorBoundary from "../ErrorBoundary";
 const Profile = () => {
   const darkTheme = useContext(ThemeContext);
   const user = useContext(UserContext);
@@ -10,19 +11,21 @@ const Profile = () => {
     color: darkTheme ? "#CCC" : "#333",
   };
   return (
-    <div style={themes} className="profile-page main">
-      {user.name === "oren" ? (
-        <>
-          <h1 className="title">Your Profile</h1>
-          <img className="pic" src={pic} alt="profile-picture" />
-          <h1 className="name">Oren Bachar</h1>
-          <section className="about">Me</section>
-          <h3 className="motto">Motto Motto</h3>
-        </>
-      ) : (
-        <Redirect to="/error" />
-      )}
-    </div>
+    <ErrorBoundary>
+      <div style={themes} className="profile-page main">
+        {user.name === "oren" ? (
+          <>
+            <h1 className="title">Your Profile</h1>
+            <img className="pic" src={pic} alt="profile-picture" />
+            <h1 className="name">Oren Bachar</h1>
+            <section className="about">Me</section>
+            <h3 className="motto">Motto Motto</h3>
+          </>
+        ) : (
+          <Redirect to="/error" />
+        )}
+      </div>
+    </ErrorBoundary>
   );
 };
 
